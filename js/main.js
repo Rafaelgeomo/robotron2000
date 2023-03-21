2347bf284bff1959ef78989dc69a1e863d74a78e
Rafaelgeomo <65200578+Rafaelgeomo@users.noreply.github.com>
2023-03-21 18:58:09 -0300

    ajuste no parseInt

--- js/main.js
+++ js/main.js
@@ -3,9 +3,9 @@ const somar = document.querySelector("#somar");
 const braco = document.querySelector("#braco");
 
 somar.addEventListener("click", (evento) => { //=> mesma coisa que escrever uma função anonima (função de seta)
-    braco.value = parceInt(braco.value) + 1;
+    braco.value = parseInt(braco.value) + 1;
 } );
 
 subtrair.addEventListener("click", (evento) => { 
-    braco.value = parceInt(braco.value) - 1;
+    braco.value = parseInt(braco.value) - 1;
 } );
